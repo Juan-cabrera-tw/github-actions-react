db78bef625537731d749c6342d1dbf0b32a6cb7b
Juan-cabrera-tw <juan.cabrera@thoughtworks.com>
2021-10-12 13:13:52 -0500

    fix: octokit bug

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -8499,13 +8499,15 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
+    const context = github.context;
     // const octokit = new github.GitHub(token);
-    const octokit = new github.getOctokit(token);
+    // const octokit = new github.(token);
+    const octokit = github.getOctokit(token);
 
-    const response = await octokit.issues.create({
+    const response = await octokit.rest.issues.create({
       // owner: github.context.repo.owner,
       // repo: github.context.repo.repo,
-      ...github.context.repo,
+      ...context.repo,
       title,
       body,
       assignees: assignees ? assignees.split("\n") : undefined,
